099df7274b64ea1c82539f794cd3bbfb58f11d58
Nickolay L. Zlobin <nickolay-z@yandex.ru>
2021-08-11 18:18:46 +0300

    Initial commit

---
+++ post.rb
@@ -0,0 +1,20 @@
+class Post
+
+    def initialize
+        @created_at = Time.now
+        @text = nil
+    end
+
+    def read_from_console
+
+    end
+
+    def to_string
+
+    end
+
+    def save
+
+    end
+
+end
